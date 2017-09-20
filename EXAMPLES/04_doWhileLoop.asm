904e556d9d09816d71d030b575c3ece9816ea9ee
Harrison117 <hsumali@up.edu.ph>
2017-09-20 16:47:21 +0800

    added loop examples =3

---
+++ EXAMPLES/04_doWhileLoop.asm
@@ -0,0 +1,73 @@
+;
+;   @author: Harold Umali (with Guidance from Ma'am Kedall Jeane Jaen)
+;   @date-created: September 20 2017
+;   @description: gets the summation of numbers 1 to N (in this example, n equals 9)
+;				using 'loop' logic (un/conditional jumps and concept of loops in high level PLs)
+;
+;       ! USES DO-WHILE LOOP CONCEPT !
+;
+
+section .data
+  sum db 0
+	N db 9
+
+	newLine db 10
+
+section .bss
+  i resb 1
+
+section .text
+  global _start
+
+_start:
+
+mov byte[i], 1				; INITIALIZATION of 'loop'
+
+loopBody:
+  mov al, [i]
+  add byte[sum], al
+  inc byte[i]					; UPDATE of the 'loop'
+
+	mov al, [N]
+	cmp byte[i], al			; CONDITION of the 'loop'
+	jle loopBody        ; if the condition holds, jump back to the label
+                      ; else... break from the 'loop'
+
+loopEnd:
+
+separationOfTwoDigits:
+	mov ah, 0
+	mov al, [sum]
+	mov bl, 10
+	div bl
+
+	mov byte[i], al			; TENS
+	mov byte[sum], ah		; ONES
+
+convertIntToChar:
+	add byte[i], '0'
+	add byte[sum], '0'
+
+printSum:
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, i
+	mov edx, 1
+	int 80h
+
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, sum
+	mov edx, 1
+	int 80h
+
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, newLine
+	mov edx, 1
+	int 80h
+
+terminate:
+	mov eax, 1
+	mov ebx, 0
+	int 80h
